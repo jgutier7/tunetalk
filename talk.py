20aae1948d56ececd8af9123f224a469818a7a60
Jacob Gutierrez <jgutier7@student05.cse.nd.edu>
2023-05-08 22:30:37 -0400

    almost done, fixing funcitionality

--- talk.py
+++ talk.py
@@ -72,12 +72,10 @@ def index():
     if request.method == 'POST':
         song = request.form.get('song')
         artist = request.form.get('artist')
+        prompt_option = request.form.get('prompt_option')
+        session['prompt_option'] = prompt_option  # Store the prompt_option in the session
 
-        if not song or not artist:
-            flash("Both song and artist must be provided")
-            return redirect(url_for('index'))
-
-        character_assumption, error = recommend_character_assumption(song, artist)
+        character_assumption, error = ai_prompt(song, artist, prompt_option)
         if error:
             flash(f"Error: {error}")
             return redirect(url_for('index'))
@@ -85,6 +83,8 @@ def index():
         return render_template('index.html', character_assumption=character_assumption)
 
     return render_template('index.html', character_assumption=None)
+
+
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -124,15 +124,30 @@ def store_songs():
     openai_function(stored_songs)
     return jsonify(status='success', message='Songs stored successfully')
 
-def recommend_character_assumption(song, artist):
+def ai_prompt(song, artist, prompt_option):
     try:
-        ''' prompt = f"If someone recommends the song '{song}' by {artist}, what assumptions can be made about their character? Write me a list of 3 songs that you'd recommend. Then say something passive aggressive about the song or the artist."
-'''
-        prompt = f"If someone recommends the song '{song}' by {artist}, what assumptions can be made about their character?"
+        prompts = {
+            '0': f"If someone recommends the song '{song}' by {artist}, what assumptions can be made about their character?",
+            '1': f"Create a short story where the song '{song}' by {artist} is the theme song.",
+            '2': f"Imagine '{song}' by {artist} as the soundtrack to a movie scene. Describe that scene in detail.",
+            '3': f"Write a fictional conversation between {artist} and another artist, discussing the creation of '{song}'.",
+            '4': f"If '{song}' by {artist} were a person, what kind of personality and appearance would they have?",
+            '5': f"Create a playlist of 5 songs that would go well with '{song}' by {artist}. Explain why you chose each song.",
+            '6': f"Write a poem inspired by the emotions or story conveyed in '{song}' by {artist}.",
+            '7': f"Design a new album cover for {artist}'s album that features '{song}'. Describe the visual elements and the concept behind it.",
+            '8': f"If '{song}' by {artist} were a dish, what ingredients and flavors would it have? Describe the dish in detail.",
+            '9': f"Imagine '{song}' by {artist} as a character in a video game. What abilities, traits, and backstory would they have?",
+            '10': f"Describe a dream music festival lineup where {artist} is performing '{song}', along with four other artists/bands. Explain why you chose each artist/band and how they complement each other."
+        }
+
+        prompt = prompts.get(prompt_option)
+        if not prompt:
+            prompt = f"If someone recommends the song '{song}' by {artist}, what assumptions can be made about their character?"
+
         response = openai.Completion.create(
             engine="text-davinci-002",
             prompt=prompt,
-            max_tokens=50,
+            max_tokens=200,
             n=1,
             stop=None,
             temperature=0.5,
@@ -142,6 +157,26 @@ def recommend_character_assumption(song, artist):
         return character_assumption, None
     except Exception as e:
         return None, str(e)
+
+
+# def recommend_character_assumption(song, artist):
+#     try:
+#         ''' prompt = f"If someone recommends the song '{song}' by {artist}, what assumptions can be made about their character? Write me a list of 3 songs that you'd recommend. Then say something passive aggressive about the song or the artist."
+# '''
+#         prompt = f"If someone recommends the song '{song}' by {artist}, what assumptions can be made about their character?"
+#         response = openai.Completion.create(
+#             engine="text-davinci-002",
+#             prompt=prompt,
+#             max_tokens=50,
+#             n=1,
+#             stop=None,
+#             temperature=0.5,
+#         )
+
+#         character_assumption = response.choices[0].text.strip()
+#         return character_assumption, None
+#     except Exception as e:
+#         return None, str(e)
 
 from spotify_auth import spotify_client
 @app.route('/spotify/callback')
@@ -165,12 +200,14 @@ def spotify_callback():
     current_user_id = user.id
     # Store the current_track in the songs table
     current_user_id = db_session.query(User).filter_by(username=session['username']).first().id
-    song_obj = Song(title=current_track['item']['name'], artist=current_track['item']['artists'][0]['name'], user_id=current_user_id)
+    prompt_option = session.get('prompt_option', None)  # Retrieve the stored prompt_option from the session
+    song_obj = Song(title=current_track['item']['name'], artist=current_track['item']['artists'][0]['name'], user_id=current_user_id, prompt_option = prompt_option)
     db_session.add(song_obj)
     db_session.commit()
 
     # Send the stored song to the OpenAI query
-    character_assumption, error = recommend_character_assumption(song_obj.title, song_obj.artist)
+    # prompt_option = session.get('prompt_option', None)  # Retrieve the stored prompt_option from the session
+    character_assumption, error = ai_prompt(song_obj.title, song_obj.artist, prompt_option)
     if error:
         flash(f"Error: {error}")
         return redirect(url_for('index'))
@@ -187,14 +224,35 @@ def spotify_assumption():
         song = last_song.title
         artist = last_song.artist
         album_cover = last_song.image_url  # Get the album cover URL from the Song object
-        character_assumption, error = recommend_character_assumption(song, artist)
+        character_assumption, error = ai_prompt(song, artist, prompt_option='0')
     else:
         song = None
         artist = None
         album_cover = None
         character_assumption = None
 
-    return render_template('spotify_assumption.html', song=song, artist=artist, album_cover=album_cover, character_assumption=character_assumption)
+    return render_template('spotify_assumption.html', song=song, artist=artist, album_cover=album_cover, character_assumption=character_assumption, prompt = '0')
+
+# @app.route('/spotify_assumption')
+# @login_required
+# def spotify_assumption():
+#     current_user_id = db_session.query(User).filter_by(username=session['username']).first().id
+#     last_song = db_session.query(Song).filter_by(user_id=current_user_id).order_by(Song.id.desc()).first()
+
+#     if last_song:
+#         song = last_song.title
+#         artist = last_song.artist
+#         album_cover = last_song.image_url  # Get the album cover URL from the Song object
+#         prompt_option = last_song.prompt_option  # Get the prompt option from the Song object
+#         character_assumption, error = ai_prompt(song, artist, prompt_option)
+#     else:
+#         song = None
+#         artist = None
+#         album_cover = None
+#         character_assumption = None
+
+#     return render_template('spotify_assumption.html', song=song, artist=artist, album_cover=album_cover, character_assumption=character_assumption)
+
 
 @app.route('/spotify/auth')
 def spotify_auth():
--- talk_classes.py
+++ talk_classes.py
@@ -23,15 +23,45 @@ class Song(Base):
     title = Column(String, nullable=False)
     artist = Column(String, nullable=False)
     genre = Column(String)
-    image_url = Column(String)  # Add this line
+    image_url = Column(String) 
+    prompt_option = Column(Integer)  
     user_id = Column(Integer, ForeignKey('users.id'))
 
-    def __init__(self, title, artist, user_id, genre=None, image_url=None):  # Update the constructor
+    def __init__(self, title, artist, user_id, prompt_option, genre=None, image_url=None):  # Update the constructor
         self.title = title
         self.artist = artist
         self.user_id = user_id
+        self.prompt_option = prompt_option  
         self.genre = genre
-        self.image_url = image_url  # Add this line
+        self.image_url = image_url 
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'artist': self.artist,
+            'genre': self.genre,
+            'image_url': self.image_url,
+            'prompt_option': self.prompt_option 
+        }
 
     def __repr__(self) -> str:
-        return f"Song(title='{self.title}', artist='{self.artist}', genre='{self.genre}', image_url='{self.image_url}')"
+        return f"Song(title='{self.title}', artist='{self.artist}', genre='{self.genre}', image_url='{self.image_url}', prompt_option={self.prompt_option})"
+
+
+# from datetime import datetime
+
+# class AIResponse(Base):
+#     __tablename__ = 'ai_responses'
+#     id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, ForeignKey('users.id'))
+#     song_id = Column(Integer, ForeignKey('songs.id'))
+#     prompt = Column(Text, nullable=False)
+#     response = Column(Text, nullable=False)
+#     created_at = Column(DateTime, default=datetime.utcnow)
+
+#     def __init__(self, user_id, song_id, prompt, response):
+#         self.user_id = user_id
+#         self.song_id = song_id
+#         self.prompt = prompt
+#         self.response = response
